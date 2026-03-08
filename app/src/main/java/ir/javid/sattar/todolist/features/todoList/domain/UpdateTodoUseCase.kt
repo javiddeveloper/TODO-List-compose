0d8de9ffb3a9bc54e902cdfdbb0a6cbd70b75032
j_sattar <javiddeveloepr@gmail.com>
2026-03-08 17:57:08 +0330

    Repackage to clean standard

--- app/src/main/java/ir/javid/sattar/todolist/features/todoList/domain/UpdateTodoUseCase.kt
+++
@@ -1,8 +0,0 @@
-package ir.javid.sattar.todolist.features.todoList.domain
-
-import ir.javid.sattar.todolist.features.todoList.data.model.TodoItem
-import kotlinx.coroutines.flow.Flow
-
-interface UpdateTodoUseCase {
-    operator fun invoke(item:TodoItem): Flow<Unit>
-}
